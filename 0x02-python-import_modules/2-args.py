83fb8546f7569514010aeeab7cccd19af54cfe1d
NjeriMaina4172 <lucymaina4172@gmail.com>
2022-12-04 19:41:23 +0300

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    count = len(argv)
+
+    if count == 1:
+        print("0 arguments.")
+    elif count == 2:
+        print("{:d} argument:".format(count - 1))
+    else:
+        print("{:d} arguments:".format(count - 1))
+    
+    for i in range(1, count):
+        print("{:d}: {:s}".format((i), argv[i]))
--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def add(a, b):
     """My addition function
 
@@ -10,6 +11,7 @@ def add(a, b):
         The return value. a + b
     """
     return (a + b)
+
 
 def sub(a, b):
     """My subtraction function
@@ -23,6 +25,7 @@ def sub(a, b):
     """
     return (a - b)
 
+
 def mul(a, b):
     """My multiplication function
 
@@ -34,6 +37,7 @@ def mul(a, b):
         The return value. a * b
     """
     return (a * b)
+
 
 def div(a, b):
     """My division function
