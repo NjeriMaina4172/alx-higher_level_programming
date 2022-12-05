3be9cafad7c7594b1a878d4bc0e4c2cfca2d16c2
NjeriMaina4172 <lucymaina4172@gmail.com>
2022-12-05 19:15:08 +0300

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,4 +9,4 @@ def print_matrix_integer(matrix=[[]]):
             print("{:d}".format(matrix[i][j]), end="")
             if j != (len(matrix[i]) - 1):
                 print(" ", end="")
-    print("")
+        print("")
