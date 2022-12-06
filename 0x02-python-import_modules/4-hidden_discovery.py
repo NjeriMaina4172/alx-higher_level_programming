5cd8bae30a4fe5a7046d5a7260302c53362ed583
NjeriMaina4172 <lucymaina4172@gmail.com>
2022-12-06 16:34:51 +0300

    compiled

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    # a program to do infinite addition
+    import hidden_4
 
-    if (len(argv) < 2):
-        print("0")
-    elif (len(argv) == 2):
-        print(argv[1])
-    else:
-        dup = [int(argv[i]) for i in range(1, len(argv))]
-        Sum = sum(dup)
-        print(Sum)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
