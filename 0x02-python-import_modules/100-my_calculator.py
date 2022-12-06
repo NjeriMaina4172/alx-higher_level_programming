f2614e72efbee5f14ad20a505e70cb1f4bd522ce
NjeriMaina4172 <lucymaina4172@gmail.com>
2022-12-06 16:50:34 +0300

    advanced

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+
+    # implementing a simple generic CLI calculator
+    if len(argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{} {} {} = {}".format(a, argv[2], b, ops[argv[2]](a, b)))
