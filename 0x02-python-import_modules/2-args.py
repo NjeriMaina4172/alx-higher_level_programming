a632dcf2a45bfc06d1f68e9b1fa89cb3cca39c50
NjeriMaina4172 <lucymaina4172@gmail.com>
2022-12-06 14:59:05 +0300

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         print("{:d} argument:".format(count - 1))
     else:
         print("{:d} arguments:".format(count - 1))
-    
+
     for i in range(1, count):
         print("{:d}: {:s}".format((i), argv[i]))
