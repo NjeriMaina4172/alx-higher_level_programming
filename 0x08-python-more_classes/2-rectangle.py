2602fa085370692805630f26c82dc4044a0b16ad
NjeriMaina4172 <lucymaina4172@gmail.com>
2023-01-03 17:55:07 +0300

    typo

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -12,7 +12,7 @@ def width(self):
 @width.setter
 def width(self, value):
     if not isinstance(value, int):
-        raise TypeError("widthe must be an integer")
+        raise TypeError("width must be an integer")
     if value < 0:
         raise ValueError("width must be >= 0")
     self.__width = value
