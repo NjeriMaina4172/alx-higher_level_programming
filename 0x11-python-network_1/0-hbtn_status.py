ae6f11488e6ba58ba84af4c5f65b243ec3a3a687
Njeri_ Maina <lucymaina4172@gmail.com>
2023-03-31 22:45:37 +0300

    adding a new line at the end

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a script that fetches https://intranet.hbtn.io/status"""
+"""fetches https://intranet.hbtn.io/status"""
 from urllib import request
 
 if __name__ == "__main__":
