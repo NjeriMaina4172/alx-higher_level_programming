5b8ba03d28e0c06861b3fd83339e1cc641ac7cef
Njeri_ Maina <lucymaina4172@gmail.com>
2023-03-31 23:58:35 +0300

    a Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-
 """
-This script takes your GitHub credentials (username and password) and uses the
-GitHub API to display your id.
-
-Usage: ./github_id.py <username> <password>
+Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
 """
 
 import requests
 import sys
 
-if __name__ == '__main__':
 
-    if len(sys.argv) != 3:
-        print("Usage: ./github_id.py <username> <password>")
+def get_github_id(username, password):
+    """
+    This function takes a username and password, and returns the user's
+    GitHub ID
+    """
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, password))
+    if response.status_code == 200:
+        return response.json().get("id")
+    else:
+        print("Error retrieving GitHub ID:", response.json().get("message"))
         sys.exit(1)
 
+
+if __name__ == "__main__":
+    # Check for the correct number of command-line arguments
+    if len(sys.argv) != 3:
+        print("Usage: python3 github_id.py <username> <token>")
+        sys.exit(1)
+
+    # Get the username and password from the command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
-    url = f'https://api.github.com/users/{username}'
 
-    response = requests.get(url, auth=(username, password))
+    # Get the user's GitHub ID
+    github_id = get_github_id(username, password)
 
-    if response.status_code == 200:
-
-        user_id = response.json().get('id')
-
-        if user_id is not None:
-            print(user_id)
-        else:
-            print('No user ID found in response.')
-            sys.exit(1)
-    else:
-        print(f'Request failed with status code {response.status_code}.')
-        sys.exit(1)
+    # Print the user's GitHub ID
+    print(github_id)
